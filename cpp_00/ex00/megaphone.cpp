c31221af01e9658b7d8ca5ebf44a8a3f21029423
İlknur Tulgar <ilknur001-@hotmail.com>
2024-12-26 00:45:45 +0300

    ex00 done

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        int i;
+        int j;
+        i = 1;
+        j = 0;
+        while (argv[i])
+        {
+            j = 0;
+            while (argv[i][j])
+            {
+                std::cout <<(char)std::toupper(argv[i][j]);
+                j++;
+            }
+            i++;
+        }
+        std::cout << std::endl;
+    }
+
+}
