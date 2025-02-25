9161ca020ffd0b5974f10bce43e4ac0c4874d34b
Ilknur Tulgar <itulgar@k1m31s03.42istanbul.com.tr>
2025-02-25 13:37:24 +0300

    ex04 finished

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,11 @@
 /*   By: itulgar <itulgar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 18:56:21 by itulgar           #+#    #+#             */
-/*   Updated: 2025/02/24 17:10:06 by itulgar          ###   ########.fr       */
+/*   Updated: 2025/02/25 13:35:24 by itulgar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <fstream>
+#include "Replace.hpp"
 
 bool is_s1(std::string filename,std::string s1)
 {
@@ -22,25 +21,44 @@ bool is_s1(std::string filename,std::string s1)
 	{
 		size_t found = line.find(s1);
 		if(found != std::string::npos)
-			return true;
+			return file.close(),true;
 		else if(!line.empty())
 			std::getline(file,line);
 		else 
-			return std::cout << "s1 not found! 🦭" << std::endl,false;
+			return file.close(),std::cout << "s1 not found! 🦭" << std::endl,false;
 	}
-	return false;
+	return file.close(),false;
 }
 
 int replace_std(std::string filename, std::string s1, std::string s2)
 {
 	if(is_s1(filename,s1))
 	{
-		
-		std::cout << "geldim usta" << std::endl;
+		std::string repStr = filename + ".replace";
+		std::ofstream fileReplace(repStr.c_str());
+		std::ifstream file(filename.c_str());
+		std::string line;
+
+		while(std::getline(file,line))
+		{
+			size_t found = line.find(s1);
+			
+			while(found != std::string::npos)
+			{
+				
+				std::string before = line.substr(0,found);
+				std::string after = line.substr(found + s1.length());
+				line = before + s2 + after;
+				found = line.find(s1,found+s2.length());
+			}
+			fileReplace << line << std::endl;
+		}
+		fileReplace.close();
+		file.close();
 		return 1;
-	}else	
+	}
+	else	
 		return 0;
-	
 }
 
 int file_check(std::string filename)
@@ -62,7 +80,7 @@ int file_check(std::string filename)
 int main(int argc,char **argv)
 {
 	if(argc != 4)
-		std::cout << "Invalid argument honey! 🦭" << std::endl;
+		std::cout << "Invalid argument! 🦭" << std::endl;
 	else
 	{
 		std::string filename = argv[1];
