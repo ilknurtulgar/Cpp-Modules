0585c1f722ecaa5475023e93a1b3a7d28e796122
İlknur Tulgar <ilknur001-@hotmail.com>
2025-08-11 16:23:03 +0300

    ex00 in cpp07 done

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -10,3 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+#define MAX_VAL 750
+
+#endif
