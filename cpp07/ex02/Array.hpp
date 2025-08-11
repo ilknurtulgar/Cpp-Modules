d0eac2167a297a7c88414494456c5e51c57fa415
İlknur Tulgar <ilknur001-@hotmail.com>
2025-08-11 19:46:55 +0300

    i will do

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -14,7 +14,30 @@
 #define ARRAY_HPP
 
 #include <iostream>
-
 #define MAX_VAL 750
+
+template <typename T>
+class Array
+{
+    private:
+        T* array;
+        unsigned int lenght;
+    public:
+        Array();
+        Array(T* array, unsigned int lenght);
+        Array(const Array& other);
+        Array& operator=(const Array& other);
+        ~Array();
+
+        //eleman erişimi
+        T& operator[](unsigned int index);
+        const T& operator[](unsigned int index) const;
+        //dizinin elaman sayısını döndürmesi için
+        unsigned int size() const;
+
+};
+
+
+
 
 #endif
