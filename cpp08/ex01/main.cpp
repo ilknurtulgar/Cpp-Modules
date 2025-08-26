fb052cc007b32ba17a0eb0b156a5ff2d90b8f4a5
Ilknur Tulgar <itulgar@k1m36s02.42istanbul.com.tr>
2025-08-26 16:40:03 +0300

    cpp08 started

---
+++ cpp08/ex01/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itulgar <itulgar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 16:09:28 by itulgar           #+#    #+#             */
+/*   Updated: 2025/08/26 16:11:49 by itulgar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+int main(){
+	
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	return 0;
+	
+}
---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itulgar <itulgar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 16:35:55 by itulgar           #+#    #+#             */
+/*   Updated: 2025/08/26 16:39:28 by itulgar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+
+template <typename T>
+class MutantStack
+{
+	private:
+		/* data */
+	public:
+		MutantStack(/* args */);
+		~MutantStack();
+};
+
+
+
+#endif
