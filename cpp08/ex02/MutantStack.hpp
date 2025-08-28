fad1cab64518efb78049d8743dabc0834949aff5
İlknur Tulgar <ilknur001-@hotmail.com>
2025-08-28 19:11:32 +0300

    ex02 in cpp08 done

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -14,17 +14,23 @@
 #define MUTANTSTACK_HPP
 
 #include <iostream>
+#include <stack>
 
 template <typename T>
-class MutantStack
+class MutantStack : public std::stack<T>
 {
-	private:
-		/* data */
 	public:
-		MutantStack(/* args */);
+		MutantStack();
+		MutantStack(const MutantStack& other);
+		MutantStack& operator=(const MutantStack& other);
 		~MutantStack();
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		iterator begin();
+		iterator end();
 };
 
-
+#include "MutantStack.tpp"
 
 #endif
