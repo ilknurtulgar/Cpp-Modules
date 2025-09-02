2af654ab2b43349b2d410111e43f687066934bdc
Ilknur Tulgar <itulgar@k1m38s02.42istanbul.com.tr>
2025-09-02 15:51:38 +0300

    i forgot try catch,the added

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,33 +6,44 @@
 /*   By: itulgar <itulgar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 16:09:28 by itulgar           #+#    #+#             */
-/*   Updated: 2025/09/02 15:38:53 by itulgar          ###   ########.fr       */
+/*   Updated: 2025/09/02 15:49:58 by itulgar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-int main(){    
-	Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-	    
-	std::cout << "sp: " << sp.shortestSpan() << std::endl;
-    std::cout << "sp: " << sp.longestSpan() << std::endl;
-    std::cout << "----------------------" << std::endl;
-    
-	Span sp2 = Span(10);
-    
-	std::vector<int> cont;
-    cont.push_back(10);
-    cont.push_back(30);
-    cont.push_back(38);    
+int main(){  
 	
-	sp2.addNumbers(cont.begin(), cont.end());    
+	try
+	{
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+			
+		std::cout << "sp: " << sp.shortestSpan() << std::endl;
+		std::cout << "sp: " << sp.longestSpan() << std::endl;
+		std::cout << "----------------------" << std::endl;
+		
+		Span sp2 = Span(10);
+    
+		std::vector<int> cont;
+		cont.push_back(10);
+		cont.push_back(30);
+		cont.push_back(38);    
+		
+		sp2.addNumbers(cont.begin(), cont.end());    
+		
+		std::cout << "sp2: " << sp2.shortestSpan() << std::endl;
+		std::cout << "sp2: " << sp2.longestSpan() << std::endl;
+		
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	  
 	
-	std::cout << "sp2: " << sp2.shortestSpan() << std::endl;
-	std::cout << "sp2: " << sp2.longestSpan() << std::endl;
 }
