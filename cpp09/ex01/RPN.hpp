1b6cf7570047b76a6c81e60630558277792f6c3f
İlknur Tulgar <ilknur001-@hotmail.com>
2025-09-24 21:40:01 +0300

    ex01 started for cpp09

--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -1,6 +1,23 @@
 #ifndef RPN_HPP
 #define RPN_HPP
 
+#include <iostream>
+#include <stack>
+
+class RPN
+{
+    private:
+        std::stack<int> container;
+    public:
+        RPN();
+        RPN(const RPN& other);
+        RPN operator=(const RPN& other);
+        ~RPN();
+
+       void execute();
+};
+
+
 
 
 #endif
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,5 +1,15 @@
 #include "RPN.hpp"
 
-int main(){
-    
+
+//> ./RPN "8 9 * 9 - 9 - 9 - 4 - 1 +"
+
+int main(int argc, char** argv){
+
+    if(argc != 2){
+        std::cout << "Error" << std::endl;
+        return;
+    }
+
+    RPN rpn;
+    rpn.execute();
 }
